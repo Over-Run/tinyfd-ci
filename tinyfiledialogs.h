25590ad5430865159eddd4b0d6e4002cfa7979e2
squid233 <60126026+squid233@users.noreply.github.com>
2025-11-22 20:03:01 +0800

    dllexport

--- tinyfiledialogs.h
+++ tinyfiledialogs.h
@@ -62,6 +62,18 @@ misrepresented as being the original software.
 #ifndef TINYFILEDIALOGS_H
 #define TINYFILEDIALOGS_H
 
+#ifndef TINYFD_EXTERN
+  #if defined(_WIN32)
+    #if defined(TINYFD_STATIC)
+      #define TINYFD_EXTERN extern
+    #else
+      #define TINYFD_EXTERN __declspec(dllexport) extern
+    #endif
+  #else
+    #define TINYFD_EXTERN extern
+  #endif
+#endif
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
@@ -72,40 +84,40 @@ extern "C" {
 #ifdef _WIN32
 /* On windows, if you want to use UTF-8 ( instead of the UTF-16/wchar_t functions at the end of this file )
 Make sure your code is really prepared for UTF-8 (on windows, functions like fopen() expect MBCS and not UTF-8) */
-extern int tinyfd_winUtf8; /* on windows char strings can be 1:UTF-8(default) or 0:MBCS */
+TINYFD_EXTERN int tinyfd_winUtf8; /* on windows char strings can be 1:UTF-8(default) or 0:MBCS */
 /* for MBCS change this to 0, in tinyfiledialogs.c or in your code */
 
 /* Here are some functions to help you convert between UTF-16 UTF-8 MBSC */
-char * tinyfd_utf8toMbcs(char const * aUtf8string);
-char * tinyfd_utf16toMbcs(wchar_t const * aUtf16string);
-wchar_t * tinyfd_mbcsTo16(char const * aMbcsString);
-char * tinyfd_mbcsTo8(char const * aMbcsString);
-wchar_t * tinyfd_utf8to16(char const * aUtf8string);
-char * tinyfd_utf16to8(wchar_t const * aUtf16string);
+TINYFD_EXTERN char * tinyfd_utf8toMbcs(char const * aUtf8string);
+TINYFD_EXTERN char * tinyfd_utf16toMbcs(wchar_t const * aUtf16string);
+TINYFD_EXTERN wchar_t * tinyfd_mbcsTo16(char const * aMbcsString);
+TINYFD_EXTERN char * tinyfd_mbcsTo8(char const * aMbcsString);
+TINYFD_EXTERN wchar_t * tinyfd_utf8to16(char const * aUtf8string);
+TINYFD_EXTERN char * tinyfd_utf16to8(wchar_t const * aUtf16string);
 #endif
 /******************************************************************************************************/
 /******************************************************************************************************/
 /******************************************************************************************************/
 
 /************* 3 funtions for C# (you don't need this in C or C++) : */
-char const * tinyfd_getGlobalChar(char const * aCharVariableName); /* returns NULL on error */
-int tinyfd_getGlobalInt(char const * aIntVariableName); /* returns -1 on error */
-int tinyfd_setGlobalInt(char const * aIntVariableName, int aValue); /* returns -1 on error */
+TINYFD_EXTERN char const * tinyfd_getGlobalChar(char const * aCharVariableName); /* returns NULL on error */
+TINYFD_EXTERN int tinyfd_getGlobalInt(char const * aIntVariableName); /* returns -1 on error */
+TINYFD_EXTERN int tinyfd_setGlobalInt(char const * aIntVariableName, int aValue); /* returns -1 on error */
 /* aCharVariableName: "tinyfd_version" "tinyfd_needs" "tinyfd_response"
    aIntVariableName : "tinyfd_verbose" "tinyfd_silent" "tinyfd_allowCursesDialogs"
 				      "tinyfd_forceConsole" "tinyfd_assumeGraphicDisplay" "tinyfd_winUtf8"
 **************/
 
-extern char tinyfd_version[8]; /* contains tinyfd current version number */
-extern char tinyfd_needs[]; /* info about requirements */
-extern int tinyfd_verbose; /* 0 (default) or 1 : on unix, prints the command line calls */
-extern int tinyfd_silent; /* 1 (default) or 0 : on unix, hide errors and warnings from called dialogs */
+TINYFD_EXTERN char tinyfd_version[8]; /* contains tinyfd current version number */
+TINYFD_EXTERN char tinyfd_needs[]; /* info about requirements */
+TINYFD_EXTERN int tinyfd_verbose; /* 0 (default) or 1 : on unix, prints the command line calls */
+TINYFD_EXTERN int tinyfd_silent; /* 1 (default) or 0 : on unix, hide errors and warnings from called dialogs */
 
 /** Curses dialogs are difficult to use and counter-intuitive.
 On windows they are only ascii and still uses the unix backslash ! **/
-extern int tinyfd_allowCursesDialogs; /* 0 (default) or 1 */
+TINYFD_EXTERN int tinyfd_allowCursesDialogs; /* 0 (default) or 1 */
 
-extern int tinyfd_forceConsole;  /* 0 (default) or 1 */
+TINYFD_EXTERN int tinyfd_forceConsole;  /* 0 (default) or 1 */
 /* for unix & windows: 0 (graphic mode) or 1 (console mode).
 0: try to use a graphic solution, if it fails then it uses console mode.
 1: forces all dialogs into console mode even when an X server is present.
@@ -116,7 +128,7 @@ extern int tinyfd_forceConsole;  /* 0 (default) or 1 */
 /* some systems don't set the environment variable DISPLAY even when a graphic display is present.
 set this to 1 to tell tinyfiledialogs to assume the existence of a graphic display */
 
-extern char tinyfd_response[1024];
+TINYFD_EXTERN char tinyfd_response[1024];
 /* if you pass "tinyfd_query" as aTitle,
 the functions will not display the dialogs
 but will return 0 for console mode, 1 for graphic mode.
@@ -129,15 +141,15 @@ for graphic mode:
 for console mode:
   dialog whiptail basicinput no_solution */
 
-void tinyfd_beep(void);
+TINYFD_EXTERN void tinyfd_beep(void);
 
-int tinyfd_notifyPopup(
+TINYFD_EXTERN int tinyfd_notifyPopup(
 	char const * aTitle, /* NULL or "" */
 	char const * aMessage, /* NULL or "" may contain \n \t */
 	char const * aIconType); /* "info" "warning" "error" */
 		/* return has only meaning for tinyfd_query */
 
-int tinyfd_messageBox(
+TINYFD_EXTERN int tinyfd_messageBox(
 	char const * aTitle , /* NULL or "" */
 	char const * aMessage , /* NULL or "" may contain \n \t */
 	char const * aDialogType , /* "ok" "okcancel" "yesno" "yesnocancel" */
@@ -145,13 +157,13 @@ int tinyfd_messageBox(
 	int aDefaultButton ) ;
 		/* 0 for cancel/no , 1 for ok/yes , 2 for no in yesnocancel */
 
-char * tinyfd_inputBox(
+TINYFD_EXTERN char * tinyfd_inputBox(
 	char const * aTitle , /* NULL or "" */
 	char const * aMessage , /* NULL or "" (\n and \t have no effect) */
 	char const * aDefaultInput ) ;  /* NULL = passwordBox, "" = inputbox */
 		/* returns NULL on cancel */
 
-char * tinyfd_saveFileDialog(
+TINYFD_EXTERN char * tinyfd_saveFileDialog(
 	char const * aTitle , /* NULL or "" */
 	char const * aDefaultPathAndOrFile , /* NULL or "" , ends with / to set only a directory */
 	int aNumOfFilterPatterns , /* 0  (1 in the following example) */
@@ -159,7 +171,7 @@ char * tinyfd_saveFileDialog(
 	char const * aSingleFilterDescription ) ; /* NULL or "text files" */
 		/* returns NULL on cancel */
 
-char * tinyfd_openFileDialog(
+TINYFD_EXTERN char * tinyfd_openFileDialog(
 	char const * aTitle, /* NULL or "" */
 	char const * aDefaultPathAndOrFile, /* NULL or "" , ends with / to set only a directory */
 	int aNumOfFilterPatterns , /* 0 (2 in the following example) */
@@ -169,12 +181,12 @@ char * tinyfd_openFileDialog(
 		/* in case of multiple files, the separator is | */
 		/* returns NULL on cancel */
 
-char * tinyfd_selectFolderDialog(
+TINYFD_EXTERN char * tinyfd_selectFolderDialog(
 	char const * aTitle, /* NULL or "" */
 	char const * aDefaultPath); /* NULL or "" */
 		/* returns NULL on cancel */
 
-char * tinyfd_colorChooser(
+TINYFD_EXTERN char * tinyfd_colorChooser(
 	char const * aTitle, /* NULL or "" */
 	char const * aDefaultHexRGB, /* NULL or "" or "#FF0000" */
 	unsigned char const aDefaultRGB[3] , /* unsigned char lDefaultRGB[3] = { 0 , 128 , 255 }; */
@@ -190,13 +202,13 @@ char * tinyfd_colorChooser(
 #ifdef _WIN32
 
 /* windows only - utf-16 version */
-int tinyfd_notifyPopupW(
+TINYFD_EXTERN int tinyfd_notifyPopupW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aMessage, /* NULL or L"" may contain \n \t */
 	wchar_t const * aIconType); /* L"info" L"warning" L"error" */
 
 /* windows only - utf-16 version */
-int tinyfd_messageBoxW(
+TINYFD_EXTERN int tinyfd_messageBoxW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aMessage, /* NULL or L"" may contain \n \t */
 	wchar_t const * aDialogType, /* L"ok" L"okcancel" L"yesno" */
@@ -205,13 +217,13 @@ int tinyfd_messageBoxW(
 		/* returns 0 for cancel/no , 1 for ok/yes */
 
 /* windows only - utf-16 version */
-wchar_t * tinyfd_inputBoxW(
+TINYFD_EXTERN wchar_t * tinyfd_inputBoxW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aMessage, /* NULL or L"" (\n nor \t not respected) */
 	wchar_t const * aDefaultInput); /* NULL passwordBox, L"" inputbox */
 
 /* windows only - utf-16 version */
-wchar_t * tinyfd_saveFileDialogW(
+TINYFD_EXTERN wchar_t * tinyfd_saveFileDialogW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aDefaultPathAndOrFile, /* NULL or L"" , ends with / to set only a directory */
 	int aNumOfFilterPatterns, /* 0 (1 in the following example) */
@@ -220,7 +232,7 @@ wchar_t * tinyfd_saveFileDialogW(
 		/* returns NULL on cancel */
 
 /* windows only - utf-16 version */
-wchar_t * tinyfd_openFileDialogW(
+TINYFD_EXTERN wchar_t * tinyfd_openFileDialogW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aDefaultPathAndOrFile, /* NULL or L"" , ends with / to set only a directory */
 	int aNumOfFilterPatterns , /* 0 (2 in the following example) */
@@ -231,13 +243,13 @@ wchar_t * tinyfd_openFileDialogW(
 		/* returns NULL on cancel */
 
 /* windows only - utf-16 version */
-wchar_t * tinyfd_selectFolderDialogW(
+TINYFD_EXTERN wchar_t * tinyfd_selectFolderDialogW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aDefaultPath); /* NULL or L"" */
 		/* returns NULL on cancel */
 
 /* windows only - utf-16 version */
-wchar_t * tinyfd_colorChooserW(
+TINYFD_EXTERN wchar_t * tinyfd_colorChooserW(
 	wchar_t const * aTitle, /* NULL or L"" */
 	wchar_t const * aDefaultHexRGB, /* NULL or L"#FF0000" */
 	unsigned char const aDefaultRGB[3], /* unsigned char lDefaultRGB[3] = { 0 , 128 , 255 }; */
